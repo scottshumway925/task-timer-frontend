40b7e7bb092f3ced9e283f5642cd746c3ada26f0
scottshumway925 <shumworld925@gmail.com>
2025-10-21 22:08:03 -0600

    Added the API key once more. It's safe, I promise.

--- firebase.js
+++ firebase.js
@@ -1,9 +1,13 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
+// Important knowledge!!!
+// This API key is going to cause GitHub to throw some errors at us. That's OK!
+// This key is meant to be publically accessable and is required in the frontend
+// code in order to work properly with the backend. 
+
 const firebaseConfig = {
-  // API Key goes here //  
-  // ...
+  apiKey: "AIzaSyB1k3ZisJHGJxYti4iuEKSW7id0m0QzYSA",
   authDomain: "assignment-time.firebaseapp.com",
   projectId: "assignment-time",
   storageBucket: "assignment-time.firebasestorage.app",
